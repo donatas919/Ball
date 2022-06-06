bb9039a1879003b432012eacdc1c6f2d9441845f
donatas <donatborkovskij@gmail.com>
2022-06-06 17:10:17 +0300

    Initial commit

---
+++ Ball/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Ball
+{
+    struct Vector
+    {
+        public int X;
+        public int Y;
+       
+        public Vector(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+    internal class Program
+    {
+        static object x = new object();
+        public static void Move()
+        {
+            lock (x)
+            {
+                Vector ballLocation = new Vector(40, 12);
+                Vector direction = new Vector(-1, -1);
+
+                while (true)
+                {
+                    Console.Clear();
+                    Console.SetCursorPosition(ballLocation.X, ballLocation.Y);
+                    Console.Write((char) 4);
+
+                    ballLocation.X += direction.X;
+                    ballLocation.Y += direction.Y;
+                    Thread.Sleep(100);
+
+                    if (ballLocation.X == 0 || ballLocation.X == 79)
+                        direction.X = -direction.X;
+                    if (ballLocation.Y == 0 || ballLocation.Y == 24)
+                        direction.Y = -direction.Y;
+                }
+            }
+        }
+        
+        public static void Main(string[] args)
+        {
+            // Thread thread = new Thread(Move);
+            // thread.IsBackground = true;
+            // thread.Start();
+
+            lock (x)
+            {
+                Move();
+            }
+           
+            Console.ReadKey(true);
+        }
+    }
+}
